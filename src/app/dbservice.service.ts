99e68d85d305442d269e01b5a1f38f4565789837
akn <javatestamarjeet@gmail.com>
2021-04-27 18:05:36 +0530

    27april

---
+++ src/app/dbservice.service.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Books } from './Books';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Customers } from './Customers';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbserviceService {
+  
+  
+
+  constructor(private con:HttpClient) { }
+
+url='http://localhost:8081';
+
+
+  servicecallbyid(value:any):Observable<Books>
+  {
+    
+    let link ="/book_id/"+value;
+    return this.con.get<Books>(this.url+link);
+  }
+
+
+  public getJSON(url: string): Observable<any> {
+    // this.startSpinner();
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    });
+    const options = { headers: headers };
+
+    return this.con.get(url, options);
+  }
+
+
+  servicecallbynameandauthor(value:any):Observable<Books>
+  {
+    
+    let link ="/bookbyname/"+value;
+    return this.con.get<Books>(this.url+link);
+  }
+  
+  servicecallforall(n:any) 
+  {
+    let link ="/bookcat/"+n;
+    return this.con.get<Books>(this.url+link);
+
+  }
+
+  //  addtocart(obj:Cart):Observable<Cart>
+  //  {
+  //   let link ="/addingcart";
+  //   return this.con.put<Cart>((this.url+link),obj);
+  //  }  
+
+
+   fetchcart()
+   {
+    //  var n=sessionStorage.getItem("user");  
+    // let link ="/fetchcart?x="+n;
+    // return this.con.get<Cart>(this.url+link);
+   }
+
+
+   booksell(b:Books):Observable<Books>
+   {
+    let link ="/addingbook";
+    return this.con.put<Books>((this.url+link),b);
+   }
+
+
+   addcust(c:Customers):Observable<Customers>
+   {
+    let link ="/addingcust";
+    return this.con.put<Customers>((this.url+link),c);
+   }
+
+   login(mobile:any):Observable<Customers>
+   { 
+    let link ="/fetchcust?x="+mobile;
+    return this.con.get<Customers>(this.url+link);
+   }
+   updateuser(user:any,m:Customers)
+   {
+    let link ="/updatecust/"+user;
+    console.log(this.url+link);
+    return this.con.put<Customers>((this.url+link),m);
+   }
+
+   deletecart(bkid,user)
+   {
+    //  console.log("called deleete");
+    // let link ="/deletecart?x="+bkid+"&y="+user;
+    // console.log(this.url+link);
+    // return this.con.get<Cart>(this.url+link);
+   }
+}
---
+++ src/app/homesearch/homesearch.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+import { Books } from '../Books';
+import { DbserviceService } from '../dbservice.service';
+
+
+
+@Component({
+  selector: 'app-homesearch',
+  template: './homesearch.component.html',
+  styleUrls: ['./homesearch.component.css']
+})
+export class HomesearchComponent implements OnInit {
+
+  constructor(private r:Router,private route:ActivatedRoute, private dbs:DbserviceService) 
+   {
+     
+   }
+
+  ngOnInit() {
+    let value=parseInt(this.route.snapshot.paramMap.get('value'));
+ // this.navvalue=value;
+  this.homenav(value);
+  }
+  //navvalue:number;
+  books:any[]=[];
+comedy:any[]=[];
+romance:any[]=[];
+sports:any[]=[];
+result:any[]=[];
+
+invalid()
+{
+  alert("Login First to Add");
+  this.r.navigate(['login']);
+}
+
+homenav(value:number)
+  {
+    if(value==1)
+    {
+      this.comedycollection();
+    }
+    else if(value==2)
+    {
+      this.biographycollection();
+    }
+    else if(value==3)
+    {
+      this.romancecollection();
+    }
+    else if(value==4)
+    {
+      this.sportscollection();
+      console.log(value);
+    }
+    else
+    {
+      this.all();
+      console.log(value);
+    }
+    console.log(value);
+   // this.navigatecheck();
+    //this.r.navigate(['homesearch']);
+  }
+  comedycollection()
+    {
+      this.result=[];
+      this.dbs.getJSON('./assets/widgets.json').subscribe(data => {
+        this.result = data;
+    });
+      console.log("Comedy Called");
+      
+    }
+
+    biographycollection()
+    {
+      
+      this.dbs.getJSON('./assets/widgets.json').subscribe(data => {
+        this.result = data;
+    });
+      console.log("biography Called");
+      console.log(this.result);
+    } 
+
+    sportscollection()
+    {
+      this.result=[];
+      console.log("Sports Called");
+    }
+    romancecollection()
+    {
+      this.result=[];
+      console.log("Romance Called");
+      for(var i=0;i<this.books.length;i++)
+        {
+          let obj=this.books[i];
+            if(obj.genre=='Romance')
+              {
+                console.log(JSON.stringify(obj));
+                this.result.push(obj);
+              }
+
+        }
+    }
+all()
+{
+  this.result=[];
+  for(var i=0;i<this.books.length;i++)
+    {
+      console.log("Working");
+      console.log(this.books.length);
+      let obj=this.books[i];
+      this.result.push(obj);
+
+    }
+}
+
+comedycollection2()
+{
+  for(var i=0;i<this.books.length;i++)
+    {
+      console.log("Working");
+      console.log(this.books.length);
+      let obj=this.books[i];
+     if(obj.genre=='Romance')
+     {
+       console.log(JSON.stringify(obj));
+     }
+
+    }
+}
+
+}
