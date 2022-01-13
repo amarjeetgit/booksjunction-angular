4bde4604a1be8944fa8fa4b9106a06fc488854dd
akn <javatestamarjeet@gmail.com>
2022-01-14 00:24:26 +0530

    14th Jan 2022 wishlist

--- src/app/homesearch/homesearch.component.ts
+++ src/app/homesearch/homesearch.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Books } from '../Books';
 import { DbserviceService } from '../dbservice.service';
 
@@ -12,124 +12,112 @@ import { DbserviceService } from '../dbservice.service';
 })
 export class HomesearchComponent implements OnInit {
 
-  constructor(private r:Router,private route:ActivatedRoute, private dbs:DbserviceService) 
-   {
-     
-   }
+  constructor(private r: Router, private route: ActivatedRoute, private dbs: DbserviceService) {
+
+  }
 
   ngOnInit() {
-    let value=parseInt(this.route.snapshot.paramMap.get('value'));
- // this.navvalue=value;
- console.log("cxame");
-  this.homenav(value);
+    let value = parseInt(this.route.snapshot.paramMap.get('value'));
+    // this.navvalue=value;
+    console.log("cxame");
+    this.homenav(value);
   }
   //navvalue:number;
-  books:any[]=[];
-comedy:any[]=[];
-romance:any[]=[];
-sports:any[]=[];
-result:any[]=[];
+  books: any[] = [];
+  comedy: any[] = [];
+  romance: any[] = [];
+  sports: any[] = [];
+  result: any[] = [];
+  wishlisted = false;
 
-invalid()
-{
-  alert("Login First to Add");
-  this.r.navigate(['login']);
-}
+  invalid() {
+    if (this.wishlisted) {
+      this.wishlisted = false;
+    }
+    else {
+      this.wishlisted = true;
+    }
+    //alert("Login First to Add");
+    // this.r.navigate(['login']);
+  }
 
-homenav(value:number)
-  {
-    if(value==1)
-    {
+  homenav(value: number) {
+    if (value == 1) {
       this.comedycollection();
     }
-    else if(value==2)
-    {
+    else if (value == 2) {
       this.biographycollection();
     }
-    else if(value==3)
-    {
+    else if (value == 3) {
       this.romancecollection();
     }
-    else if(value==4)
-    {
+    else if (value == 4) {
       this.sportscollection();
       console.log(value);
     }
-    else
-    {
+    else {
       this.all();
       console.log(value);
     }
     console.log(value);
-   // this.navigatecheck();
+    // this.navigatecheck();
     //this.r.navigate(['homesearch']);
   }
-  comedycollection()
-    {
-      this.result=[];
-      this.dbs.getJSON('./assets/widgets.json').subscribe(data => {
-        this.result = data;
+  comedycollection() {
+    this.result = [];
+    this.dbs.getJSON('./assets/widgets.json').subscribe(data => {
+      this.result = data;
     });
-      console.log("Comedy Called");
-      
-    }
+    console.log("Comedy Called");
 
-    biographycollection()
-    {
-      
-      this.dbs.getJSON('./assets/widgets.json').subscribe(data => {
-        this.result = data;
+  }
+
+  biographycollection() {
+
+    this.dbs.getJSON('./assets/widgets.json').subscribe(data => {
+      this.result = data;
     });
-      console.log("biography Called");
-      console.log(this.result);
-    } 
+    console.log("biography Called");
+    console.log(this.result);
+  }
 
-    sportscollection()
-    {
-      this.result=[];
-      console.log("Sports Called");
-    }
-    romancecollection()
-    {
-      this.result=[];
-      console.log("Romance Called");
-      for(var i=0;i<this.books.length;i++)
-        {
-          let obj=this.books[i];
-            if(obj.genre=='Romance')
-              {
-                console.log(JSON.stringify(obj));
-                this.result.push(obj);
-              }
+  sportscollection() {
+    this.result = [];
+    console.log("Sports Called");
+  }
+  romancecollection() {
+    this.result = [];
+    console.log("Romance Called");
+    for (var i = 0; i < this.books.length; i++) {
+      let obj = this.books[i];
+      if (obj.genre == 'Romance') {
+        console.log(JSON.stringify(obj));
+        this.result.push(obj);
+      }
 
-        }
     }
-all()
-{
-  this.result=[];
-  for(var i=0;i<this.books.length;i++)
-    {
+  }
+  all() {
+    this.result = [];
+    for (var i = 0; i < this.books.length; i++) {
       console.log("Working");
       console.log(this.books.length);
-      let obj=this.books[i];
+      let obj = this.books[i];
       this.result.push(obj);
 
     }
-}
+  }
 
-comedycollection2()
-{
-  for(var i=0;i<this.books.length;i++)
-    {
+  comedycollection2() {
+    for (var i = 0; i < this.books.length; i++) {
       console.log("Working");
       console.log(this.books.length);
-      let obj=this.books[i];
-     if(obj.genre=='Romance')
-     {
-       console.log(JSON.stringify(obj));
-     }
+      let obj = this.books[i];
+      if (obj.genre == 'Romance') {
+        console.log(JSON.stringify(obj));
+      }
 
     }
-}
+  }
 
 }
